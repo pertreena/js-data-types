7688978ea750c186098880ffc15a9f0ac783f2fb
pertreena <trealeef@yahoo.com>
2018-05-30 15:55:10 +0800

    exercisemd done

---
+++ script.js
@@ -0,0 +1,46 @@
+typeof(15)
+//Prediction: number
+//Actual: number
+typeof(5.5)
+//Prediction: number
+//Actual: number
+typeof(NaN)
+//Prediction: Falsey
+//actual: undefined/number
+typeof('Hello')
+//Prediction: string
+//actual: string
+typeof(true)
+//Prediction: boolean
+//actual: boolean
+typeof(1 != 2)
+//Prediction: boolean
+//actual: boolean
+'hamburger' + 's'
+//Prediction: hamburgers
+//actual: hamburgers
+
+'hamburgers' - 's'
+//Prediction: undefined
+//actual: NaN
+
+'1' + '3'
+//Prediction: '13'
+//Actual: '13'
+
+'1' - '3'
+//Prediction: NaN
+//Actual: -2
+
+"johnny" + 5
+//Prediction: johnny5
+//Actual: johnny5
+
+"johnny" - 5
+//Prediction: NaN
+//Actual: NaN
+
+99 * "luftbaloons"
+//Prediction: NaN
+//Actual: NaN
+
